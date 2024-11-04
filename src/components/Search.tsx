8c7dcf2ecb78d2224c51ad38c7dcd8f4712de511
andyLyon-212 <locopandy@gmail.com>
2024-11-04 11:15:31 -0700

    uplaoad component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,9 +6,11 @@ import { TextFieldValidation, TextFieldClientProps } from 'payload'
 import { text } from 'payload/shared'
 import type { ClientCollectionConfig } from 'payload'
 
-const colorMissingOctothorpRegex = new RegExp("^([A-Fa-f0-9]{8}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{4}|[A-Fa-f0-9]{3})$");
+const colorMissingOctothorpRegex = new RegExp(
+  '^([A-Fa-f0-9]{8}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{4}|[A-Fa-f0-9]{3})$',
+)
 
- export const Search: React.FC<TextFieldClientProps> = ({ ...props }) => {
+export const Search: React.FC<TextFieldClientProps> = ({ ...props }) => {
   const { value, setValue } = useField<string>({})
 
   // Mutating the value in a validation handler....
@@ -21,31 +23,27 @@ const colorMissingOctothorpRegex = new RegExp("^([A-Fa-f0-9]{8}|[A-Fa-f0-9]{6}|[
     return text(value, options)
   }
 
-  const collectionSlug = 'pages';
+  const collectionSlug = 'pages'
   const { getEntityConfig } = useConfig()
   const collectionConfig = getEntityConfig({ collectionSlug }) as ClientCollectionConfig
-   console.log('collectionConfig',collectionConfig);
-   console.log('collectionConfig',collectionConfig.fields);
+  console.log('collectionConfig', collectionConfig)
+  console.log('collectionConfig', collectionConfig.fields)
 
   return (
-    <div className={ 'flex items-center gap-6' }>
-      <div
-        className={ 'color-swatch w-32 h-32' }
-        style={{ backgroundColor: value || '#00000000' }}
-      />
-      <TextField
-        { ...props }
-        validate={validateText}
-      />
+    <div className={'flex items-center gap-6'}>
+      <div className={'color-swatch w-32 h-32'} style={{ backgroundColor: value || '#00000000' }} />
+      <TextField {...props} validate={validateText} />
       <Button
-        aria-label='Create New'
+        aria-label="Create New"
         buttonStyle="pill"
-        onClick={()=>{console.log('hello')}}
+        onClick={() => {
+          console.log('hello')
+        }}
         size="small"
-        >
-         Create New
-       </Button>
-      < ListControls collectionConfig={collectionConfig} fields={collectionConfig.fields} />
+      >
+        Create New
+      </Button>
+      <ListControls collectionConfig={collectionConfig} fields={collectionConfig.fields} />
     </div>
   )
 }
